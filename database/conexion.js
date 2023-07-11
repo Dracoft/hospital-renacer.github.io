84b8f9dd47436eb9f4c0cddaf350de03bcc48f2f
Incógnita X <julian.cortes43@gmail.com>
2023-07-10 21:20:51 -0500

    Update conexion.js

--- database/conexion.js
+++ database/conexion.js
@@ -3,13 +3,13 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',//aqui va la contraseña de workbench
-    database: 'veterinaria',
-    charset: 'utf8mb4' // Establecer el conjunto de caracteres en UTF-8
+    password: 'root',
+    database: 'hospital',
+    charset: 'utf8mb4'
 });
 
 
-connection.connect((error) => { // Conectar a la base de datos
+connection.connect((error) => {
     if (error) {
         console.error('Error al conectar a la base de datos: ', error);
     } else {
@@ -17,4 +17,4 @@ connection.connect((error) => { // Conectar a la base de datos
     }
 });
 
-module.exports = { connection }; // Exportar la función para obtener usuarios
+module.exports = { connection };
